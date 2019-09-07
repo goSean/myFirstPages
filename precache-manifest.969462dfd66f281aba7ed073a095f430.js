c9da16adffef7190c8c09e3547a0b797319f83a0
goSean <shenguox9011@gmail.com>
2019-09-07 15:52:45 +0800

    Updates

---
+++ precache-manifest.969462dfd66f281aba7ed073a095f430.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dc0b0f03d234edfdd05b9263fab5c11",
+    "url": "/myFirstPages/index.html"
+  },
+  {
+    "revision": "021e542ee495b892ec6f",
+    "url": "/myFirstPages/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "31fefca5c6291a72dbd6",
+    "url": "/myFirstPages/static/js/2.ae086021.chunk.js"
+  },
+  {
+    "revision": "021e542ee495b892ec6f",
+    "url": "/myFirstPages/static/js/main.aaf135ab.chunk.js"
+  },
+  {
+    "revision": "f5e625ffa71c1839a170",
+    "url": "/myFirstPages/static/js/runtime~main.d739dc32.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myFirstPages/static/media/logo.5d5d9eef.svg"
+  }
+]);
